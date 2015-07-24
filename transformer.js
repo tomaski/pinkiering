882d5dcd6d1ea9be4bed377437ffad6190008b66
David Ford <david@smithandrobot.com>
2015-07-24 14:57:09 -0500

    updated copy added project images

---
+++ transformer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var stdin = process.openStdin();
+var data = "";
+
+stdin.on('data', function(chunk) {
+  data += chunk;
+});
+
+stdin.on('end', function() {
+  process.stdout.write(data.replace('</body>', '  <script src="http://localhost:9091/livereload.js"></script>\n</body>'));
+});
