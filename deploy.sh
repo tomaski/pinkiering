a148f0635cea82fe0d4bec86ac1a10abe171ef53
David Ford <david@smithandrobot.com>
2015-07-24 23:05:53 -0500

    updated deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+git commit -am'pre-deploy commit'
+git checkout -b gh-pages
+ls | grep -v node_modules | grep -v dev | xargs rm -rf
+mv dev/* ./
+git add --all
+git commit -am'automated'
+git push origin gh-pages --force
+git checkout master
+git branch -D gh-pages
