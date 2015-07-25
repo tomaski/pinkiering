7d36a94657beea25486ea69a34b2c7a1cbe7c37e
David Ford <david@smithandrobot.com>
2015-07-25 18:55:55 -0500

    quit trying to get my hosting provider to cname support, moving the site to (mt). Changed deploy script, added Goggle analytics

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-git commit -am'pre-deploy commit'
-git checkout -b gh-pages
+
+# 
+# Some var set up. Less typing
+#
+builddir=./build/*
+
+# Wrap this in a string so '~' doesn't resolve to local users home directory
+proddir='~/domains/pinkiering.com/html/'
+user=pinkiering.com
+server=pinkiering.com
+
+# EOF Didn't work here. 
+# Had to wrap the remote commands in a string :/
+ssh $user@$server '
+zipdate=$(date +%m-%d-%Y-%H-%M)
+cd ~/domains/pinkiering.com/html/
+zip -r ../backups/$zipdate.zip ./
+exit
+'
+# Build the prod files and mins them
 npm run build
-ls | grep -v node_modules | grep -v build | xargs rm -rf
-mv ./build/* ./
-git add --all
-git commit -am'automated'
-git push origin gh-pages --force
-rm -rf ./build
-git checkout master
-git branch -D gh-pages
+
+# Put them on the server
+rsync --ignore-times -r --exclude '.DS_Store' $builddir $user@$server:$proddir
+
+# Clean up (delete) the build directory
+npm run build:clean
