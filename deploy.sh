ab6bfb556124b0beadfdf79a0e018823d46c5e39
David Ford <david@smithandrobot.com>
2015-07-25 00:04:07 -0500

    pre-deploy commit

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git commit -am'pre-deploy commit'
 git checkout -b gh-pages
 npm run build
 ls | grep -v node_modules | grep -v dev | xargs rm -rf
-mv build/* ./
+mv ./build/* ./
 git add --all
 git commit -am'automated'
 git push origin gh-pages --force
