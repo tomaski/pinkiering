1ecda79355a6edd5f793b468b262935600f026ea
David Ford <david@smithandrobot.com>
2015-07-27 13:12:53 -0500

    removing relative paths since not pushing to gh-pages

--- src/js/site.js
+++ src/js/site.js
@@ -1,6 +1,5 @@
 var formValidator = require('./form-validator');
 var form = document.querySelector('form');
-var particles = require('./particles');
 
 // Validates contact form
 formValidator.create(form);
