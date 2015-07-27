477c8f3a69f44bf447b93b0de078e7ec6c396825
David Ford <david@smithandrobot.com>
2015-07-27 10:42:52 -0500

    removed headline

--- deploy.sh
+++ deploy.sh
@@ -15,17 +15,22 @@ server=pinkiering.com
 # EOF Didn't work here. 
 # Had to wrap the remote commands in a string :/
 # TODO - remove old backups after (n) backups
+echo 'Zipping & backing up current site...'
 ssh $user@$server '
 zipdate=$(date +%m-%d-%Y-%H-%M)
 cd ~/domains/pinkiering.com/html/
 zip -r ../backups/$zipdate.zip ./
 exit
 '
+
 # Build and mins the prod files. See package.json
+echo 'Building site...'
 npm run build
 
 # Put built files on the server
+echo 'Deploying files to server...'
 rsync --ignore-times -r --exclude '.DS_Store' $builddir $user@$server:$proddir
 
 # Clean up (delete) the build directory
+echo 'Cleaning up...'
 npm run build:clean
--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,6 @@
 var formValidator = require('./form-validator');
 var form = document.querySelector('form');
+var particles = require('./particles');
 
 // Validates contact form
 formValidator.create(form);
